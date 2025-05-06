68f4a634f2aae297523cdf892f11bdd12fb7081d
sadhin28 <taosifsadhin113@gmail.com>
2025-05-06 18:16:58 +0600

    add header

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,13 +2,13 @@
 const Navbar = ({freeClaim}) => {
     return (
         <div className="">
-            <div class="place-items-center px-6 items-center sticky top-0 rounded-b-2xl navbar bg-base-300 font-bold grid lg:flex lg:justify-between md:flex md:justify-between md:items-center lg:text-2xl md:text-1xl">
+            <div class="gap-5 place-items-center px-6 items-center  rounded-b-2xl navbar bg-base-300 font-bold grid lg:flex lg:justify-between md:flex md:justify-between md:items-center lg:text-2xl md:text-1xl">
                
                 <img className="w-12 lg:w-20 md:w-15 mx-auto lg:mx-0 md:mx-0" src="/src/assets/logo.png" alt="" />
                 
-                <ul className="mx-auto lg:mx-0 md:mx-0 lg:flex md:flex items-center gap-5">
-                         <div className="items-center flex gap-5 lg:gap-6 md:gap-4 mx-auto lg:mx-0 md:mx-0 ">
-                         <li>Home</li>
+                <ul className="mx-auto gap-5 lg:mx-0 md:mx-0 lg:flex md:flex items-center ">
+                        <div className="  items-center flex gap-5 lg:gap-6 md:gap-4 mx-auto lg:mx-0 md:mx-0 ">
+                        <li>Home</li>
                         <li>Fixture</li>
                         <li>Teams</li>
                         <li>Schedules</li>
