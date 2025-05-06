e49f58204285f291dafc554559a37be6afaa130c
sadhin28 <taosifsadhin113@gmail.com>
2025-05-06 20:06:04 +0600

    add coin from json

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,27 @@ import HeroSection from './Components/HeroSection/HeroSection'
 import Navbar from './Components/Navbar/Navbar'
 
 function App() {
+
+    //fetch allimg json file
+    const [allimg,setAllimg]=useState(arguments);
+    const [coins,setCoins]=useState(arguments);
+    useEffect(()=>{
+       fetch('Allimg.json')
+       .then(res=>res.json())
+       .then(images=>{
+          setAllimg(images)
+         
+       })
+    },[])
+    
+   useEffect(()=>{
+     fetch('Coin.json')
+     .then(res=>res.json())
+     .then(coin=>{
+        setCoins(coin)
+     })
+   },[])
+
   //increment Claim free credit
   const [freeClaim,setfreeClaim]=useState(0)
 
@@ -11,20 +32,12 @@ function App() {
     const newfreeclaim=freeClaim + 50000;
     setfreeClaim(newfreeclaim)
   }
-  //fetch json file
-  const [allimg,setAllimg]=useState(arguments);
-  useEffect(()=>{
-     fetch('Allimg.json')
-     .then(res=>res.json())
-     .then(images=>{
-        setAllimg(images)
-     })
-  },[])
+
   return (
     <div className='max-w-11/12 mx-auto'>
-       <Navbar allimg={allimg} freeClaim={freeClaim}></Navbar>
+       <Navbar allimg={allimg} coins={coins} freeClaim={freeClaim}></Navbar>
        <div className=''>
-          <HeroSection handelfreeClaim={handelfreeClaim}></HeroSection>
+          <HeroSection  handelfreeClaim={handelfreeClaim}></HeroSection>
        </div>
     </div>
   )
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,12 @@
+const Navbar = ({freeClaim,allimg,coins}) => {
+    const [logo]=allimg
+    const [coin]=coins
 
-const Navbar = ({freeClaim,allimg}) => {
-     
-    return (
-        <div className="">
+      return(
+        <div>
             <div className="gap-5 place-items-center px-6 items-center  rounded-b-2xl navbar bg-base-300 font-bold grid lg:flex lg:justify-between md:flex md:justify-between md:items-center lg:text-2xl md:text-1xl">
                
-                <img className="w-12 lg:w-20 md:w-15 mx-auto lg:mx-0 md:mx-0" src={allimg[0].logo} alt="" />
+                <img className="w-12 lg:w-20 md:w-15 mx-auto lg:mx-0 md:mx-0" src={logo.logo} alt="" />
                 
                 <ul className="mx-auto gap-5 lg:mx-0 md:mx-0 lg:flex md:flex items-center ">
                         <div className="  items-center flex gap-5 lg:gap-6 md:gap-4 mx-auto lg:mx-0 md:mx-0 ">
@@ -14,7 +15,7 @@ const Navbar = ({freeClaim,allimg}) => {
                         <li>Teams</li>
                         <li>Schedules</li>
                          </div>
-                        <p className="text-center"><small className="  border-2 rounded-2xl place-items-center  w-40 md:w-50 p-2 mx-auto  flex justify-center">{freeClaim} Coin <img className="w-6" src="/src/assets/653278_coin_bitcoin_cash_currency_dollar_icon.ico" alt="" /></small></p>
+                        <p className="text-center"><small className="  border-2 rounded-2xl place-items-center  w-40 md:w-50  mx-auto  flex justify-center">{freeClaim} Coin <img className="w-4 mx-2 " src={coin.coin} alt="" /></small></p>
                 </ul>
                 
                
