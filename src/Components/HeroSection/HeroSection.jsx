e30d6b861a59da95558de58bec4fdb64478c637b
sadhin28 <taosifsadhin113@gmail.com>
2025-05-06 17:34:31 +0600

    responsive claim section

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -5,7 +5,7 @@ const HeroSection = () => {
             <div className='flex  flex-col justify-center lg:max-w-330 md:max-w-200 mx-auto rounded-4xl bg-blend-color-burn lg:h-100 md:h-80 h-60' id="heroSection">
                 <img className='w-40 lg:w-60 md:w-50 p-5 mx-auto' src="/src/assets/banner-main.png" alt="" />
                 <h1 className='font-bold'>Assemble My Ultimate Dream 11 Cricket Team</h1>
-                <button className=' font-black rounded-xl btn mb-6 hover:bg-lime-200 mt-4 w-50 mx-auto text-center bg-[#E7FE29]'>Claim Free Credit</button>
+                <button className=' p-6 font-black rounded-xl btn mb-6 hover:bg-lime-200 mt-4  mx-auto text-center bg-[#E7FE29]'>Claim Free Credit</button>
               
             </div>
         </div>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,16 +2,16 @@
 const Navbar = () => {
     return (
         <div className="">
-            <div class=" items-center sticky top-0 rounded-b-2xl navbar bg-base-300 font-bold grid lg:flex lg:justify-between md:flex md:justify-between md:items-center px-10 text-sm lg:text-2xl md:text-1xl">
+            <div class="px-6 items-center sticky top-0 rounded-b-2xl navbar bg-base-300 font-bold grid lg:flex lg:justify-between md:flex md:justify-between md:items-center text-sm lg:text-2xl md:text-1xl">
                
                 <img className="w-12 lg:w-20 md:w-15 mx-auto lg:mx-0 md:mx-0" src="/src/assets/logo.png" alt="" />
                 
-                <ul className="flex gap-3 lg:gap-6 md:gap-4 mx-auto lg:mx-0 md:mx-0 ">
+                <ul className=" items-center flex gap-5 lg:gap-6 md:gap-4 mx-auto lg:mx-0 md:mx-0 ">
                         <li>Home</li>
                         <li>Fixture</li>
                         <li>Teams</li>
                         <li>Schedules</li>
-                        <li className="flex border px-1 border-blue-700 rounded-3xl">{600} coin <img
+                        <li className="flex border px-1 items-center border-blue-700 rounded-3xl">{600} coin <img
                         className="w-6"
                          src="/src/assets/653278_coin_bitcoin_cash_currency_dollar_icon.ico" alt="" /></li>
                 </ul>
