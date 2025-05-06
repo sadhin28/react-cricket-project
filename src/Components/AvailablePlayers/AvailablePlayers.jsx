21dde9457ac4808fc73e4be6e5f35a6b864f871e
sadhin28 <taosifsadhin113@gmail.com>
2025-05-07 01:27:24 +0600

    add mx-5

--- src/Components/AvailablePlayers/AvailablePlayers.jsx
+++ src/Components/AvailablePlayers/AvailablePlayers.jsx
@@ -3,8 +3,8 @@ import SelectedPlayers from '../SelectedPlayers/SelectedPlayers';
 import './AvailablePlayer.css'
 const AvailablePlayers = ({handleisActive,isactive,allplayers}) => {
     return (
-        <div >
-            <div className=" mt-10 mb-10 flex items-center font-black justify-between text-sm lg:text-2xl md:text-xl">
+        <div className='mx-5' >
+            <div className="  mt-10 mb-10 flex items-center font-black justify-between text-sm lg:text-2xl md:text-xl">
             <p>Available Players</p>
             <div className="join ">
                 <button onClick={()=>handleisActive('available')} className={`${isactive.available?'btn active':'btn'} font-black p-5 rounded-tl-2xl rounded-bl-2xl`}>Available</button>
