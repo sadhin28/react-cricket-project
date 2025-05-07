9533a3bc4a43b206e86300176addd91e47821f3e
sadhin28 <taosifsadhin113@gmail.com>
2025-05-07 14:58:38 +0600

    add responsive

--- src/Components/Player/Player.jsx
+++ src/Components/Player/Player.jsx
@@ -6,7 +6,7 @@ const Player = ({ allplayers,handelSelectedPlayers }) => {
     return (
         <div>
            
-            <div className="card bg-base-100 w-96 shadow-sm">
+            <div className="card bg-base-100 w-86 shadow-sm">
                 <figure>
                     <img
                         src={image_url}
