8215e9039b4cce3a8cfc389c41d4e20f1116c61a
sadhin28 <taosifsadhin113@gmail.com>
2025-05-08 19:05:24 +0600

    project name update

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -6,7 +6,7 @@ const HeroSection = ({handelfreeClaim,heroPhoto}) => {
         <div id='hero' className='mt-10 text-center bg-base-100 '>
             <div className='flex  flex-col justify-center lg:max-w-330 md:max-w-200 mx-auto rounded-xl bg-blend-color-burn lg:h-100 md:h-80 h-60' id="heroSection">
                 <img className='w-40 lg:w-60 md:w-50 p-5 mx-auto' src={heroimg.heroimg} alt="" />
-                <h1 className='font-bold lg:text-2xl md:text-xl'><small>Assemble My Ultimate Dream 11 Cricket Team</small></h1>
+                <h1 className='font-bold lg:text-2xl md:text-xl'><small>BPL-DREAM 11</small></h1>
             
                 <button onClick={handelfreeClaim} className=' p-6 font-black rounded-xl btn mb-6 hover:bg-lime-200 mt-4  mx-auto text-center bg-[#E7FE29]'>Claim Free Credit</button>
                
